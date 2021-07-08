d40e49faaeaa15a1f6c9506addfc032a73034f31
Someshbose <iamsomeshbose@gmail.com>
2021-07-08 14:46:13 +0530

    Unisex Bathroom problem,SynchronousTask Exector & merge sort added

---
+++ src/main/java/interview/SynchronousExecutor.java
@@ -0,0 +1,57 @@
+package interview;
+
+interface Callback {
+    public void done();
+}
+
+class Executor {
+
+    public void asynchronousExecution(Callback callback) throws Exception {
+        Thread t = new Thread(() -> {
+            // Do some useful work
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ie) {
+
+            }
+            callback.done();
+        });
+        t.start();
+    }
+}
+
+
+public class SynchronousExecutor extends Executor {
+
+    @Override
+    public void asynchronousExecution(Callback callback) throws Exception {
+        Object signal = new Object();
+        final boolean[] isDone = new boolean[1];
+        Callback cb = new Callback() {
+            @Override
+            public void done() {
+                callback.done();
+                synchronized (signal) {
+                    signal.notify();
+                    isDone[0] = true;
+                }
+            }
+        };
+        // Call the asynchronous executor
+        super.asynchronousExecution(cb);
+        synchronized (signal) {
+            while (!isDone[0]) {
+                signal.wait();
+            }
+        }
+    }
+
+    public static void main( String args[] ) throws Exception {
+        SynchronousExecutor executor = new SynchronousExecutor();
+        executor.asynchronousExecution(() -> {
+            System.out.println("I am done");
+        });
+        System.out.println("main thread exiting...");
+    }
+}
+
